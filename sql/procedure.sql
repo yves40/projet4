6617301e19144ce87db533feceb898772de2dc07
yves40 <yves.toubhans@free.fr>
2022-06-20 00:06:47 +0200

    Slowly learn procedure variables

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -1,12 +1,24 @@
 use yves;
-drop procedure if exists yves.generateBillets;
-
 delimiter //
-create procedure yves.generateBillets(in thecount int)
+
+drop procedure if exists yves.findFirstAuthorID;
+create procedure yves.findFirstAuthorID(out authorid int, out authoremail varchar(128))
   begin
-    select email from yves.users order by email;
+    select id, email into authorid, authoremail from yves.users
+      where userrole = 'AUTHOR' limit 1;
+  end//
+
+drop procedure if exists yves.findFirstReaderID;
+create procedure yves.findFirstReaderID(out readerid int)
+  begin
+    select id into readerid from yves.users 
+      where userrole = 'READER' limit 1;
   end//
 
 delimiter ;
-call yves.generateBillets(12);
+call yves.findFirstAuthorID(@A, @AA);
+select @A 'First Author', @AA 'With mail' ;
+call yves.findFirstReaderID(@B);
+select @B 'First Reader';
+
 
