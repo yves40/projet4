7673a869bfd7f30b51f56a03e3f6d9ab261ed34b
yves40 <yves.toubhans@free.fr>
2022-06-20 17:33:03 +0200

    Still learning procs

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -5,7 +5,7 @@ drop procedure if exists yves.findFirstAuthorID;
 create procedure yves.findFirstAuthorID(out authorid int, out authoremail varchar(128))
   begin
     select id, email into authorid, authoremail from yves.users
-      where userrole = 'AUTHOR' limit 1;
+      where userrole = 'AUTHOR' limit 1;    
   end//
 
 drop procedure if exists yves.findFirstReaderID;
@@ -15,10 +15,17 @@ create procedure yves.findFirstReaderID(out readerid int)
       where userrole = 'READER' limit 1;
   end//
 
+drop procedure if exists yves.log;
+create procedure yves.log(in mess varchar(128), in payload varchar(128))
+  begin
+    select concat_ws(':','LOG', mess, payload) '';
+  end//
 delimiter ;
+
+
 call yves.findFirstAuthorID(@A, @AA);
-select @A 'First Author', @AA 'With mail' ;
+call yves.log('First Author ID is', @A);
 call yves.findFirstReaderID(@B);
-select @B 'First Reader';
+call yves.log('First Reader ID is', @B);
 
 
